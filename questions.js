def64062609e293d6bb335745594a85c2e4825c0
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 23:19:07 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:" ....... 6 June",
+        options:["on", "at", "in"],
+        answer: 0
+    },
+    {
+        q:" ....... the evening",
+        options:["at", "in", "on"],
+        answer: 1
+    },
+    {
+        q:" ....... half past two",
+        options:["on", "in", "at"],
+        answer: 2
+    },
+    {
+        q:" ....... Wednesday",
+        options:["on", "at", "in" ],
+        answer: 0
+    },
+    {
+        q:" ....... 2007",
+        options:["on", "in", "at"],
+        answer: 1
+    },
+    {
+        q:" ....... September",
+        options:["at", "on", "in"],
+        answer: 2
+    },
+    {
+        q:"....... 24 September",
+        options:["on", "in", "at"],
+        answer: 0   
+    },
+    {
+        q:"....... Thursday",
+        options:["at", "on", "in"],
+        answer: 1
+    },
+    {
+        q:"....... 11.45",
+        options:["on", "in", "at"],
+        answer: 2
+    },
+    {
+        q:" ....... Christmas Day",
+        options:["on", "at", "in"],
+        answer: 0
+    }
+
+]
